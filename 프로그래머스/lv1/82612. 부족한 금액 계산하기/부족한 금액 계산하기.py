1f5a7a4e148e8d3e3f64fd67e6ddf296dedd9319
yujeong-world <124220083+yujeong-world@users.noreply.github.com>
2023-07-17 22:23:31 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -0,0 +1,9 @@
+def solution(price, money, count):
+    total = 0
+    for i in range(1,count+1):
+        total += price*i
+    if total>money:
+        answer = total - money
+    else:
+        answer = 0
+    return answer

02144e15993b20b49b1a4bc64130c2fc970cea48
yujeong kwon <124220083+yujeong-world@users.noreply.github.com>
2024-05-20 17:42:39 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int solution(int num1, int num2) {
+        int answer = num1-num2;
+        return answer;
+    }
+    public static void main(String[] args){
+        System.out.printf("%d", solution(2,3));
+        System.out.printf("%d", solution(100,2));
+    }
+}
